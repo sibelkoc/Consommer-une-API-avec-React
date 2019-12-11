ff8576c8090548c375beb5e70c7ee3c6bb46aceb
Sibel Koc <sibel.koc.pro@outlook.fr>
2019-12-11 15:43:38 +0100

    fin quete 07

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DisplayEmployee from './components/DisplayEmployee';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const sampleEmployee = {
+  character: 'Dr. Nick',
+  quote: 'Ah, be creative. Instead of making sandwhiches with bread, use Pop-Tarts. Instead of chewing gum, chew bacon.',
+  image: 'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNickRiviera.png?1497567511084',
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employee: sampleEmployee
+    };
+    this.getEmployee = this.getEmployee.bind(this);
+  }
+
+  getEmployee() {
+    axios.get('https://thesimpsonsquoteapi.glitch.me/quotes')
+      .then(response => response.data)
+      .then(data => {
+        console.log(data);
+        this.setState({
+          employee: data[0],
+        });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <DisplayEmployee employee={this.state.employee} />
+        <button type="button" onClick={this.getEmployee}>Get Quote</button>
+      </div>
+    );
+  }
 }
 
 export default App;
